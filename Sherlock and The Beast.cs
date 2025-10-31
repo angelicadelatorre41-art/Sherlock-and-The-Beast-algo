f7047a2b5b9a18d77d05d45d1a23944d93a0b65e
angelicadelatorre41-art <angelicadelatorre41@gmail.com>
2025-10-31 10:57:43 +0800

    Sherlock and The Beast.cs

---
+++ Sherlock and The Beast.cs
@@ -0,0 +1,57 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'decentNumber' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void decentNumber(int n)
+    {
+         int fives = n;
+
+        while (fives % 3 != 0)
+        {
+            fives -= 5;
+        }
+
+        if (fives < 0)
+        {
+            Console.WriteLine("-1");
+            return;
+        }
+
+        Console.WriteLine(new string('5', fives) + new string('3', n - fives));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Result.decentNumber(n);
+        }
+    }
+}
